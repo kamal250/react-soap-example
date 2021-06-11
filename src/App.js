1e1a35967c3ff786bd44aceb0983b9c6d9a26e30
Kamal Joshi <kamal.joshi@iflair.com>
2021-06-11 20:08:10 +0530

    WIP :: SOAP request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const soapConfig = {
+    headers: {
+      'Content-Type': 'text/xml'      
+    }
+  };
+  const soapXmlBeginPart = `<soapenv:Envelope 
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
+  xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" 
+  xmlns:urn="urn:Magento">
+  <soapenv:Header/>`;
+  const soapXmlAuthXmlBody = `
+  <soapenv:Body>
+  <urn:login soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <username xsi:type="xsd:string">${process.env.REACT_APP_SOAPUSER}</username>
+  <apiKey xsi:type="xsd:string">${process.env.REACT_APP_SOAPPASS}</apiKey>
+  </urn:login>
+  </soapenv:Body>`;
+  const soapXmlEndPart = `</soapenv:Envelope>`;
+
+  const soapAuthXml = soapXmlBeginPart + soapXmlAuthXmlBody + soapXmlEndPart;
+  const authorize = () => {
+    axios.post(
+      process.env.REACT_APP_SOAPURL,
+      soapAuthXml,
+      soapConfig
+    ).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.          
         </p>
         <a
           className="App-link"
@@ -17,6 +51,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => authorize()}>Authorize</button>
       </header>
     </div>
   );
